0a4031ce253c9190888ddfbf187a0d68fe553d8b
Sarah Harun <sharun@bd.imshealth.com>
2015-06-04 18:11:01 +0600

    control id method

--- Merck Sereno Dashboard/Dashboard/DashboardComponent/Models/FilterItem.cs
+++ Merck Sereno Dashboard/Dashboard/DashboardComponent/Models/FilterItem.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections.Generic;
+using DashboardFramework.Web.Convensions;
 
 namespace Dashboard.DashboardComponent.Models
 {
     public class FilterItem
     {
+        private readonly HtmlContainerIdConvention _convetion = new HtmlContainerIdConvention();
+
         public string Name { get; set; }
         public string Label { get; set; }
-        public string ControlId { get; set; }
+        
         public string ViewId { get; set; }
         public string ModifyParam { get; set; }
         public List<string> HasParamDependency { get; set; }
+
+        private string _controlId;
+        public string ControlId
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_controlId))
+                {
+                    return string.Format(_convetion.GetFilterControlId(Name));
+                }
+                return _controlId;
+            }
+            set { _controlId = value; }
+        }
     }
 }
